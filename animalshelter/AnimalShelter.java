873fa4f85044bfd9e9020f849fb322b0e818d105
amayaschalmers <amary@student.chalmers.se>
2025-02-10 20:06:13 +0100

    ok

--- animalshelter/AnimalShelter.java
+++ animalshelter/AnimalShelter.java
@@ -6,19 +6,19 @@ import java.util.List;
 /**
  * Created by Niklas on 2016-02-07.
  */
-public class AnimalShelter {
+public class AnimalShelter<T> {
     // TODO task 2b: This is the class you primarily need to update, using
     //  parametric polymorphism, to ensure that shelters can be specialised
     //  to specific animals, with complete static type guarantees.
     //  .
     //  Hint: the subclasses CatShelter and DogShelter should become obsolete.
-    private List<Animal> animalsInShelter = new ArrayList<>(20);
+    private List<T> animalsInShelter = new ArrayList<>(20);
 
-    Animal adoptAnimal(){
+    T adoptAnimal(){
         return animalsInShelter.remove(0);
     }
 
-    void putAnimal(Animal animal){
+    void putAnimal(T animal){
         animalsInShelter.add(animal);
     }
 
--- likeinhaskell/Tuple.java
+++ likeinhaskell/Tuple.java
@@ -8,7 +8,31 @@ package likeinhaskell;
 //  (possibly) different (arbitrary) types, and have the methods fst()
 //  and snd() to return the respective components.
 //  NOTE: The class should be read-only after initial construction.
-public class Tuple {
-    // public ... fst(){...}
-    // public ... snd(){...}
+public class Tuple<T1, T2> {
+    T1 t1;
+    T2 t2;
+
+    public Tuple(T1 value1, T2 value2) {
+        this.t1 = value1;
+        this.t2 = value2;
+    }
+
+    public T1 fst() {
+        return this.t1;
+    }
+
+    public T2 snd() {
+        return this.t2;
+    }
+
+
+}
+
+class Testcode{
+    public static void main(String [] args) {
+        Tuple<String, Integer> myTuple = new Tuple<>("Abc", 2);
+        System.out.println(myTuple.t1);
+        System.out.println(myTuple.t2);
+        System.out.println();
+    }
 }
